3a79a4e9664c9e4b60cbb5914f29c84ba8d46c95
shreyaprabhakar7 <64889083+shreyaprabhakar7@users.noreply.github.com>
2020-05-26 12:08:33 +0530

    Update export_inference_graph.py

--- inference/export_inference_graph.py
+++ inference/export_inference_graph.py
@@ -1,17 +1,5 @@
-# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
+# command =  python export_inference_graph.py --input_type image_tensor --pipeline_config_path training/faster_rcnn_inception_v2_pets.config --trained_checkpoint_prefix training/model.ckpt-XXXX --output_directory inference_graph
+# Here, in place of XXXX, use the last saved checkpoint of model training.
 
 r"""Tool to export an object detection model for inference.
 
